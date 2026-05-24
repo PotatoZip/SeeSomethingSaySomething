3a280181d032aa8caa1297c0f48c617aab50b0ed
Piotr Trojan <Piotr Trojan>
2026-05-24 12:25:32 +0200

    Updates

---
+++ assets/Card-BqjvOq_i.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-D2IznH-l.js";function t({children:r,className:e="",...o}){return s.jsx("div",{className:`bg-white rounded-2xl shadow-sm border border-slate-200 overflow-hidden ${e}`,...o,children:r})}export{t as C};
